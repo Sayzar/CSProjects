c913f2ea09585914c6990eea98aa6024f10c06b5
Cesar Ramirez <cesarramirez93@gmail.com>
2014-03-09 18:57:42 -0700

    No change
    No Change

--- CPUSchedulingSimulator/src/com/jimweller/cpuscheduler/PrioritySchedulingAlgorithm.java
+++ CPUSchedulingSimulator/src/com/jimweller/cpuscheduler/PrioritySchedulingAlgorithm.java
@@ -8,20 +8,25 @@
  */
 package com.jimweller.cpuscheduler;
 
+import java.util.Vector;
+
 public class PrioritySchedulingAlgorithm extends BaseSchedulingAlgorithm implements OptionallyPreemptiveSchedulingAlgorithm {
     private boolean preemptive;
-
+    private Vector<Process> jobs;
+    
     PrioritySchedulingAlgorithm(){
+    	preemptive = isPreemptive();
+    	jobs = new Vector<Process>();
     }
 
     /** Add the new job to the correct queue.*/
     public void addJob(Process p){
-
+    	jobs.add(p);
     }
     
     /** Returns true if the job was present and was removed. */
     public boolean removeJob(Process p){
-
+    	return jobs.remove(p);
     }
 
     /** Transfer all the jobs in the queue of a SchedulingAlgorithm to another, such as
@@ -33,7 +38,7 @@ public class PrioritySchedulingAlgorithm extends BaseSchedulingAlgorithm impleme
 
     /** Returns the next process that should be run by the CPU, null if none available.*/
     public Process getNextJob(long currentTime){
-
+    	return null;
     }
 
     public String getName(){
