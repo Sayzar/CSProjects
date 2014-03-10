b27f49b624c339bb3448988b97dfbf59f5eb1c92
Cesar Ramirez <cesarramirez93@gmail.com>
2014-03-09 18:41:10 -0700

    FCFS Alg
    Finished FCFS Algorithm, check later to optimize code

--- CPUSchedulingSimulator/src/com/jimweller/cpuscheduler/FCFSSchedulingAlgorithm.java
+++ CPUSchedulingSimulator/src/com/jimweller/cpuscheduler/FCFSSchedulingAlgorithm.java
@@ -12,31 +12,45 @@ import java.util.*;
 
 public class FCFSSchedulingAlgorithm extends BaseSchedulingAlgorithm {
 
+	private Vector<Process> jobs;
+	
     FCFSSchedulingAlgorithm(){
-
+    	activeJob = null;
+    	jobs = new Vector<Process>();
     }
 
     /** Add the new job to the correct queue.*/
     public void addJob(Process p){
-
+    	jobs.add(p);
     }
     
     /** Returns true if the job was present and was removed. */
     public boolean removeJob(Process p){
-
+    	return jobs.remove(p);
     }
 
     /** Transfer all the jobs in the queue of a SchedulingAlgorithm to another, such as
 	when switching to another algorithm in the GUI */
     public void transferJobsTo(SchedulingAlgorithm otherAlg) {
+    	for (int i = jobs.size()-1; i >= 0; i--) {
+    	    Process job = jobs.get(0);
+    	    this.removeJob(job);
+    	    otherAlg.addJob(job);
+    	}
     }
 
 
     public boolean shouldPreempt(long currentTime){
+    	return false;
     }
 
     /** Returns the next process that should be run by the CPU, null if none available.*/
     public Process getNextJob(long currentTime){
+    	if(jobs.size() == 0)
+    		return null;
+    	else
+    		activeJob = jobs.firstElement();    	
+    	return activeJob;
     }
 
     public String getName(){
